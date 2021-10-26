ae7918ce2fc94351f72d0c6439197524d63e20a3
botdev <devcase@icloud.com>
2021-10-26 14:31:55 +0000

    update

--- index.js
+++ index.js
@@ -4,40 +4,61 @@ const request = require('request-json');
 // const http = require('http');
 const axios = require('axios')
 
+// 上报url
 var chat_url = "https://chat.digis.cyou/hooks/d45YqyJfyeTDKHLR8/ZtvpNExiFBisKmrZmwR35i3qecNS7MbThttdruCiJQKQoJgb";
 
-// var client = request.createClient(chat_url);
-
-
 var dataUrl = null
-
-let hotNews = null;
+var hotNews = null
+var checkedCoins = []
 const webSite = 'https://www.binance.com'
 const url = 'https://www.binance.com/en/support/announcement'
 
-superagent.get(url).end((err, res) => {
-  if (err) {
-    // 如果访问失败或者出错，会这行这里
-    console.log(`热点新闻抓取失败 - ${err}`)
-  } else {
-    // 访问成功，请求http页面所返回的数据会包含在res
-    // 抓取热点新闻数据
-    hotNews = getHotNews(res)
-    console.log(hotNews);
+axios
+  .post(chat_url, { text: "开始爬取检测coin in BN ，检测间隔为12s"})
 
-    axios
-      .post(chat_url, dataUrl)
-      .then(res => {
-        console.log(`statusCode: ${res.status}`)
-        console.log(res)
-      })
-      .catch(error => {
-        console.error(error)
-      })
+// 定时执行间隔
+setInterval(intervalRequestSite, 12500);
 
-  }
-});
+function intervalRequestSite() {
 
+  superagent.get(url).end((err, res) => {
+    if (err) {
+      // 如果访问失败或者出错，会这行这里
+      console.log(`BN新闻抓取失败 - ${err}`)
+      errorInfo = { text: "BN新闻抓取失败" }
+
+      axios
+        .post(chat_url, errorInfo)
+        .then(res => {
+          console.log(`statusCode: ${res.status}`)
+          // console.log(res)
+        })
+        .catch(error => {
+          console.error(error)
+        })
+    } else {
+      // 访问成功，请求http页面所返回的数据会包含在res
+      // 抓取热点新闻数据
+      hotNews = getHotNews(res)
+      // console.log(hotNews);
+      console.log(checkedCoins);
+
+      if (checkedCoins.includes(hotNews) == false) {
+        checkedCoins.push(hotNews)
+        axios
+          .post(chat_url, dataUrl)
+          .then(res => {
+            console.log(`statusCode: ${res.status}`)
+            // console.log(res)
+          })
+          .catch(error => {
+
+            console.error(error)
+          })
+      }
+    }
+  });
+}
 
 let getHotNews = (res) => {
   // 访问成功，请求http页面所返回的数据会包含在res.text中。
@@ -48,7 +69,9 @@ let getHotNews = (res) => {
 
   let con = $('div#__APP').find('a')
   let news = con.nextAll()
+  // t 是触发条件的截取文字
   var t = null
+  // 获得每一个元素，并检索
   news.each(function () {
 
     let text = $(this).text()
@@ -58,18 +81,10 @@ let getHotNews = (res) => {
 
       dataUrl = {
         text: " 币安上新 \n " + t + "\n" + webSite + $(this).attr('href'),
-      };
-
-
-
-      // client.post('', data, function (err, res, body) {
-      //   console.log(res.statusCode, body);
-      // });
+      }
     }
-
   })
-  console.log(t)
+  // console.log(t)
   return t
-
 };
 
