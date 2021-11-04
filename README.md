61691dd66b8fb6ed5c5207b7fa80f82a0ad4450e
botdev <devcase@icloud.com>
2021-11-04 07:56:21 +0000

    fix bug

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ npm install
 
 启动
 npm start
+
+部署（需要安装pm2）
+pm2 start app.js
+
+或
+npm run pub
--- index.js
+++ index.js
@@ -40,7 +40,7 @@ function intervalRequestSite() {
       // 访问成功，请求http页面所返回的数据会包含在res
       // 抓取热点新闻数据
       hotNews = getHotNews(res)
-      // console.log(hotNews);
+      console.log(hotNews);
       console.log(checkedCoins);
 
       if (checkedCoins.includes(hotNews) == false) {
@@ -67,17 +67,21 @@ let getHotNews = (res) => {
    */
   let $ = cheerio.load(res.text);
 
-  let con = $('div#__APP').find('a')
-  let news = con.nextAll()
+  let con = $('#__APP').find('a')
+  // let con = $('body').find('a')
+  let news = con.contents()
+  // console.log(news)
+
   // t 是触发条件的截取文字
-  var t = null
+  var t = "没找到新list"
   // 获得每一个元素，并检索
   news.each(function () {
 
     let text = $(this).text()
+    console.log(text)
     if (text.match('Binance Will List')) {
       t = $(this).text()
-      // console.log(webSite + $(this).attr('href'))
+      console.log(webSite + $(this).attr('href'))
 
       dataUrl = {
         text: " 币安上新 \n " + t + "\n" + webSite + $(this).attr('href'),
